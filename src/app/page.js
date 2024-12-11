5b8da6690e02f9a8df4b97a5d13949141ee082e3
azaaa-9 <az95342030@gmail.com>
2024-12-11 18:08:44 +0800

    + search button

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,29 @@
+"use client"
+import { SearchIcon } from "./componant/searchIcon";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      
+
+
+  <div className="w-full h-screen flex bg-(#404040) justify-around py-40 bg-auto">
+
+
+    <div className="w-[20%] h-[800px] bg-stone-50 rounded-xl italic text-black flex items-start"> 
+
+    <SearchIcon/>
+    <input
+      type="text"
+      placeholder="Search City..."
+      class="w-full px-4 py-2 pr-12 text-sm border border-gray-300 rounded-full shadow-sm focus:outline-none focus:ring-2 focus:ring-cyan-300 focus:border-cyan-300"
+    />
+
     </div>
+
+
+    <div className="w-[20%] h-[800px] bg-slate-900 rounded-xl">asfasfas</div>
+  </div>
   );
 }
+
+
