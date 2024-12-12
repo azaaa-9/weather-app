29a1c36d0f6a82f1e64246ef6fa1d464284d13ac
azaaa-9 <az95342030@gmail.com>
2024-12-12 18:25:17 +0800

    card componant icon+img

--- src/app/componant/card.js
+++ src/app/componant/card.js
@@ -1,22 +1,48 @@
 "use client"
+
 import { Location } from "./location"
-export function Card(props) {
-    const Card = ({value, temperature}) => {
-        const nightCardColors = `bg-[#111827bf] bg-gradient-to-b from-[#111827] to-[#1f2937] text-white shadow-[#111827]`;
-        const colors = value === `night` ? nightCardColors : `bg-[59FAFB]`;
-      }
+
+const Card = ({value}) => {
+    const sun =  "SUN.png"
+    const img = value === "day" ? "SUN.png" : "moon.png" 
+  const color = value === "day" ? "bg-white" : "gray"
+    
 
     return(
 
-    <div className="w-[450px] h-[800px] absolute left-[193px] top-[216px] rounded-[60px] bg-slate-200">
-        <div className="text-green-400 font-mono my-1  ">
-            <div className="">December 13, 2024</div>
-            <div className="text-[48px]"> Krakow</div>
+    <div className="w-[450px] h-[800px] absolute left-[193px] top-[216px] rounded-[60px] bg-slate-200 space-y-28 ">
+
+
+        <div className="text-green-400 font-mono my-1 flex  place-content-around mt-[30px]">
+
             <div>
-            <Location/>
+            <p>December 13, 2024</p>
+            <p className="text-4xl">Krakov</p>
             </div>
-        </div>
-    </div>
+            
+            <Location/>
+            
+            </div>
+
+
+            <div>
+            <img className="items-center justify-center" src={img}/>
+            </div>
+            
+
+
+            <div className="flex gap-6 text-3xl justify-around text-black mt-auto space-x-3 opacity-60  ">
+            <i className="fa-solid fa-house"></i>
+            <i className="fa-solid fa-location-dot"></i>
+            <i className="fa-solid fa-heart"></i>
+            <i className="fa-solid fa-user"></i>
+            </div>
+
+
+            </div>
+    
 
     )
 }
+
+export {Card}
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 "use client"
 
-import Image from "next/image";
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import { Location } from "./componant/location";
 import { Card } from "./componant/card";
 import { SearchIcon } from "./componant/SearchIcon";
@@ -9,44 +9,44 @@ import { SearchIcon } from "./componant/SearchIcon";
 export default function Home() {
   return (
 
-<div className="bg-black h-screen">
-  <div className="bg-[#f3f4f6] h-[1200px] w-[1600px] flex justify-between mx-auto">
-    <div className="bg-[#f3f4f6] h-[1200px] w-[800px] relative">
-      <Card value = 'day'/>
+    <div className="bg-black h-screen">
+      <div className="bg-[#f3f4f6] h-[1200px] w-[1600px] flex justify-between mx-auto">
+        <div className="bg-[#f3f4f6] h-[1200px] w-[800px] relative">
+          <Card value='day'/>
 
-      
 
-      <div className="z-10 w-[200px] h-[200px] bg-[#f3f4f6] rounded-[50%] absolute left-[700px] top-[500px] flex">
-        <img className="w-[43px] h-[86px] ml-[50px] my-[50px]"
-        src="/left.png"/>
-         <img className="w-[43px] h-[86px] ml-[50px] my-[50px]"
-        src="/right.png"/>
+
+          <div className="z-10 w-[200px] h-[200px] bg-[#f3f4f6] rounded-[50%] absolute left-[700px] top-[500px] flex">
+            <img className="w-[43px] h-[86px] ml-[50px] my-[50px]"
+              src="/left.png" />
+            <img className="w-[43px] h-[86px] ml-[50px] my-[50px]"
+              src="/right.png" />
+          </div>
+
+
+          <div className="ml-20 flex px-4 py-3 rounded-md border-2  hover:border-white overflow-hidden max-w-md mx-auto font-[sans-serif]">
+            <SearchIcon />
+            <input type="text" placeholder="Search City..." className="border-none outline-none p-2 w-64 rounded-lg text-black" />
+          </div>
+
+
+        </div>
+
+        {/* <div className="background-1"></div> */}
+
+        <div className="relative">
+          <Card value='night' />
+          <div className="w-[800px] h-[500px] bg-[#0f141e] rounded-bl-[25px]"></div>
+          <div className="w-[800px] h-[200px] bg-[#0f141e] "></div>
+          <div className="w-[800px] h-[500px] bg-[#0f141e] rounded-tl-[25px]">
+
+
+          </div>
+        </div>
+
       </div>
-
-
-      <div className="ml-20 flex px-4 py-3 rounded-md border-2  hover:border-white overflow-hidden max-w-md mx-auto font-[sans-serif]">
-     <SearchIcon/>
-        <input type="text" placeholder="Search City..." className="border-none outline-none p-2 w-64 rounded-lg text-black" />
-      </div>
-
 
     </div>
-
-    <div className="background-1"></div>
-
-     <div className="relative">
-         <Card value = 'night'/>
-  <div className="w-[800px] h-[500px] bg-[#0f141e] rounded-bl-[25px]"></div>
-  <div className="w-[800px] h-[200px] bg-[#0f141e] "></div>
-  <div className="w-[800px] h-[500px] bg-[#0f141e] rounded-tl-[25px]">
-
-   
-  </div>
-  </div>
- 
- </div>
-
-  </div>
 
 
   );
