f9804ab66dc50a0594f3c754289310214f212337
azaaa-9 <az95342030@gmail.com>
2024-12-12 21:49:54 +0800

    search button +

--- src/app/componant/card.js
+++ src/app/componant/card.js
@@ -2,13 +2,16 @@
 
 import { Location } from "./location"
 
+
+
 const Card = ({value}) => {
     const sun =  "SUN.png"
     const img = value === "day" ? "SUN.png" : "moon.png" 
   const color = value === "day" ? "bg-white" : "gray"
-    
+
 
     return(
+ 
 
     <div className="w-[450px] h-[800px] absolute left-[193px] top-[216px] rounded-[60px] bg-slate-200 space-y-28 ">
 
@@ -17,18 +20,23 @@ const Card = ({value}) => {
 
             <div>
             <p>December 13, 2024</p>
-            <p className="text-4xl">Krakov</p>
+            <p className="text-4xl">Krakow</p>
             </div>
             
             <Location/>
             
             </div>
+       
 
-
-            <div>
-            <img className="items-center justify-center" src={img}/>
+            <div className="flex flex-col items-center">
+            <img className="w-52 h-52" src={img}/>
             </div>
             
+
+            {/* <div className="flex justify-center items-center min-h-screen bg-gray-700">
+              <Card value="day" temperature={26} />
+              <Card value="night" temperature={17} />
+            </div> */}
 
 
             <div className="flex gap-6 text-3xl justify-around text-black mt-auto space-x-3 opacity-60  ">
---
+++ src/app/componant/weather.js
@@ -0,0 +1,14 @@
+// "use client"
+
+// const { useState } = require("react")
+
+// function Weather() {
+// const [data, setData] = useState({});
+// const [location, setLocation] = useState("");
+
+//     const API_KEY = "f21b4c899cccd9e952907f3ed4257ac0"
+//     const url = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=${API_key}`
+//     return(
+
+//     )
+// }
