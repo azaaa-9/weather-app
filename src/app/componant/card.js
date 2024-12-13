2b2f888d8256e307c995898f057268b06e6ccddf
azaaa-9 <az95342030@gmail.com>
2024-12-13 16:20:53 +0800

    search button zassan

--- src/app/componant/card.js
+++ src/app/componant/card.js
@@ -5,22 +5,24 @@ import { Location } from "./location"
 
 
 const Card = ({value}) => {
-    const sun =  "SUN.png"
-    const img = value === "day" ? "SUN.png" : "moon.png" 
-  const color = value === "day" ? "bg-white" : "gray"
+    const sun =  "rain.png"
+    const img = value === "day" ? "rain.png" : "rain (1).png" 
+    const nightCardColors =
+    "bg-[111827bf] bg-gradient-to-b from-[#1F2937] to-[#11182700] text-white shadow-[#111827]";
+    const colors = value === "day" ? "bg-[#ffffff] text-black" : nightCardColors;
+    const nightIconColor = "text-white"
+    const iconColor = value === "day" ? "text-black" : nightIconColor
 
 
     return(
  
 
-    <div className="w-[450px] h-[800px] absolute left-[193px] top-[216px] rounded-[60px] bg-slate-200 space-y-28 ">
+    <div className={`w-[450px] h-[800px] absolute left-[193px] top-[216px] rounded-[60px] ${colors} space-y-52`}>
 
-
-        <div className="text-green-400 font-mono my-1 flex  place-content-around mt-[30px]">
-
+        <div className={`font-manrope my-1 flex  place-content-around  mt-[30px]`}>
             <div>
             <p>December 13, 2024</p>
-            <p className="text-4xl">Krakow</p>
+            <p className="text-6xl">Krakow</p>
             </div>
             
             <Location/>
@@ -31,20 +33,14 @@ const Card = ({value}) => {
             <div className="flex flex-col items-center">
             <img className="w-52 h-52" src={img}/>
             </div>
-            
-
-            {/* <div className="flex justify-center items-center min-h-screen bg-gray-700">
-              <Card value="day" temperature={26} />
-              <Card value="night" temperature={17} />
-            </div> */}
 
 
-            <div className="flex gap-6 text-3xl justify-around text-black mt-auto space-x-3 opacity-60  ">
-            <i className="fa-solid fa-house"></i>
-            <i className="fa-solid fa-location-dot"></i>
-            <i className="fa-solid fa-heart"></i>
-            <i className="fa-solid fa-user"></i>
-            </div>
+            <div className={`flex gap-6 text-3xl justify-around  mt-auto space-x-3 ${iconColor} opacity-60`}>
+              <i className="fa-solid fa-house"></i>
+              <i className="fa-solid fa-location-dot"></i>
+              <i className="fa-solid fa-heart"></i>
+              <i className="fa-solid fa-user"></i>
+              </div>
 
 
             </div>
--- src/app/page.js
+++ src/app/page.js
@@ -1,108 +1,3 @@
-// "use client"
-
-// import '@fortawesome/fontawesome-free/css/all.min.css';
-// import { Location } from "./componant/location";
-// import { Card } from "./componant/card";
-// import { SearchIcon } from "./componant/SearchIcon";
-// import { useState } from 'react';
-// import axios from 'axios';
-
-
-
-// export default function Home() {
-//   const [city, setCity] = useState("");       
-//   const [weatherData, setWeatherData] = useState(null); 
-//   const [error, setError] = useState(""); 
-//   const API_KEY = 'f1b42bb5fd693c21eb6b8e3b5a55299b'
-//   const url = `"https://api.openweathermap.org/data/2.5/weather"`
-
-
-//   const searchCityWeather = async () => {
-//     if (!city) {
-//       setError("Please enter a city name.");
-//       return;
-//     }
-//     try {
-//       const response = await axios.get(baseUrl, {
-//         params: {
-//           q: city,
-//           appid: apiKey,
-//           units: "metric",  // You can use "imperial" for Fahrenheit if you want
-//         }
-//       });
-
-//       setWeatherData(response.data); 
-//     }
-
-//   return (
-
-//     <div className="bg-black h-screen">
-//       <div className="bg-[#f3f4f6] h-[1200px] w-[1600px] flex justify-between mx-auto">
-//         <div className="bg-[#f3f4f6] h-[1200px] w-[800px] relative">
-//           <Card value='day'/>
-
-
-
-//           <div className="z-10 w-[200px] h-[200px] bg-[#f3f4f6] rounded-[50%] absolute left-[700px] top-[500px] flex ">
-//             <img className="w-[43px] h-[86px] ml-[50px] my-[50px]"
-//               src="/left.png" />
-//             <img className="w-[43px] h-[86px] ml-[50px] my-[50px]"
-//               src="/right.png" />
-//           </div>
-
-
-//           <div className="ml-20 flex px-4 py-3 rounded-md border-2  hover:border-white overflow-hidden max-w-md mx-auto font-[sans-serif]">
-//             <SearchIcon />
-//             <input 
-//             type="text"
-//              placeholder="Search City..." 
-//              className="border-none outline-none p-2 w-64 rounded-lg text-black"
-//              value={city}
-//               onChange={(e) => setCity(e.target.value)}
-//              />
-//                 <button
-//               onClick={searchCityWeather}  // Call the search function on click
-//               className="ml-2 px-4 py-2 bg-blue-500 text-white rounded-lg"
-//             >
-//               Search
-//             </button>
-//           </div>
-//    {/* Display Weather Data */}
-//    {loading && <div className="text-white mt-4">Loading...</div>}
-//           {error && <div className="text-red-500 mt-4">{error}</div>}
-//           {weatherData && (
-//             <div className="mt-4 text-white">
-//               <h2 className="text-2xl">{weatherData.name}</h2>
-//               <p>{weatherData.weather[0].description}</p>
-//               <p>{weatherData.main.temp}°C</p>
-//               <p>Humidity: {weatherData.main.humidity}%</p>
-//             </div>
-//           )}
-//         </div>
-
-
-//         </div>
-
-//         {/* <div className="background-1"></div> */}
-
-//         <div className="relative">
-//           <Card value='night' />
-//           <div className="w-[800px] h-[500px] bg-[#0f141e] rounded-bl-[25px]"></div>
-//           <div className="w-[800px] h-[200px] bg-[#0f141e] "></div>
-//           <div className="w-[800px] h-[500px] bg-[#0f141e] rounded-tl-[25px]">
-
-
-//           </div>
-//         </div>
-
-//       </div>
-
-//     </div>
-
-
-//   );
-// }
-
 "use client"
 
 import '@fortawesome/fontawesome-free/css/all.min.css';
@@ -173,7 +68,7 @@ export default function Home() {
           
         
           {weatherData && (
-            <div className="mt-4 text-black">
+            <div className="mt-4 text-black text-center">
               <h2 className="text-2xl">{weatherData.name}</h2>
               <p>{weatherData.weather[0].description}</p>
               <p>{weatherData.main.temp}°C</p>
@@ -183,7 +78,7 @@ export default function Home() {
 
         
         <div className="relative">
-          <Card value='night' />
+        <Card value='night'/>
           <div className="w-[800px] h-[500px] bg-[#0f141e] rounded-bl-[25px]"></div>
           <div className="w-[800px] h-[200px] bg-[#0f141e] "></div>
           <div className="w-[800px] h-[500px] bg-[#0f141e] rounded-tl-[25px]"></div>
